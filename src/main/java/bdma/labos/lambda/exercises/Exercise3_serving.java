a6aa863b098d895f9ed7c69f70156b08f8c2913a
Jordi Casanovas <tote88@gmail.com>
2018-06-22 20:36:33 +0200

    ass time to ex1 and start ex3

--- src/main/java/bdma/labos/lambda/exercises/Exercise3_serving.java
+++ src/main/java/bdma/labos/lambda/exercises/Exercise3_serving.java
@@ -1,6 +1,14 @@
 package bdma.labos.lambda.exercises;
 
 
+import bdma.labos.lambda.writers.WriterClient;
+import bdma.labos.lambda.writers.WriterServer;
+import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.spark.MongoSpark;
+import org.bson.Document;
 
 public class Exercise3_serving {
 
@@ -8,10 +16,25 @@ public class Exercise3_serving {
 								
 		/*********************/
 		//insert your code here 
-		
-		
-		
-		
+
+		MongoClient client = new MongoClient();
+		MongoDatabase database = client.getDatabase("twitter");
+		MongoCollection<Document> sentimentCollection = database.getCollection("twitter_sentiment");
+		MongoCollection<Document> summaryCollection = database.getCollection("twitter_summary");
+
+		FindIterable<Document> sentimentDocuments = sentimentCollection.find();
+		FindIterable<Document> summaryDocuments = summaryCollection.find();
+
+		for (Document doc : sentimentDocuments) {
+			System.out.println("S " + doc.toJson());
+		}
+
+		for (Document doc : summaryDocuments) {
+			System.out.println("W " + doc.toJson());
+		}
+
+
+
 		/*********************/
 		
 	}
